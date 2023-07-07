8f3164f3891cf530945791141d4a724a732e0fb3
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-07-07 13:21:44 -0500

    write solutions

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    fibonacci = []
+
+    for i in range(0, length):
+        fibonacci.append(i if i < 2 else fibonacci[-1] + fibonacci[-2])
+
+    print(fibonacci)
